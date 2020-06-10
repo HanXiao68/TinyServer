50549e65a7701d6b0add37c4255f661ae96c4b4d
HanXiao <41134571+HanXiao68@users.noreply.github.com>
2020-06-10 16:39:54 +0800

    Update threadpool.h

--- threadPool/threadpool.h
+++ threadPool/threadpool.h
@@ -1,34 +1,152 @@
-## 半同步半反应堆线程池
-### 线程池
----
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
 
+#include <list>
+#include <cstdio>
+#include <exception>
+#include <pthread.h>
+#include "../lock/locker.h"
+#include "../CGImysql/sql_connection_pool.h"
+
+template <typename T>
+class threadpool
+{
 public:
-        构造器（线程池中线程的数量，最多等待的连接请求数量）    
-        析构器    
-        append()添加请求连接。
+    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
+    threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
+    ~threadpool();
+    bool append(T *request, int state);
+    bool append_p(T *request);
 
-private: 
-woker() 工作线程运行的函数，不断从工作队列中取出任务，并执行。    
-run( )
+private:
+    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
+    static void *worker(void *arg);
+    void run();
 
-变量：线程池中线程数  
-        请求队列中允许的最大请求数 
-       
- ---
-### 半同步/半反应堆线程池原理
-
----
-
-主线程接受工作任务，并插入到工作队列中，睡眠在工作队列上的线程通过竞争获取执行权。
-实现了主线程和工作线程的解耦。
-
-
----
-
-### 线程池的作用
-
----
-* 空间换时间，用服务器的资源来换取运行时间效率的减少。
-* 池是一组相同类型的资源的集合，这组资源在服务器启动的时候，就别创建好。称为静态资源。
-* 当服务器进入正式运行时，开始处理客户的请求时，如果需要相关的资源，可以直接从池中获取，无需动态分配。
-* 服务器使用完一个线程之后，将这个线程放回线程池中，不用执行系统调用来释放资源。
+private:
+    int m_thread_number;        //线程池中的线程数
+    int m_max_requests;         //请求队列中允许的最大请求数
+    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
+    std::list<T *> m_workqueue; //请求队列
+    locker m_queuelocker;       //保护请求队列的互斥锁
+    sem m_queuestat;            //是否有任务需要处理
+    connection_pool *m_connPool;  //数据库
+    int m_actor_model;          //模型切换
+};
+template <typename T>
+threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
+{
+    if (thread_number <= 0 || max_requests <= 0)
+        throw std::exception();
+    m_threads = new pthread_t[m_thread_number];
+    if (!m_threads)
+        throw std::exception();
+    for (int i = 0; i < thread_number; ++i)
+    {
+        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+        if (pthread_detach(m_threads[i]))
+        {
+            delete[] m_threads;
+            throw std::exception();
+        }
+    }
+}
+template <typename T>
+threadpool<T>::~threadpool()
+{
+    delete[] m_threads;
+}
+template <typename T>
+bool threadpool<T>::append(T *request, int state)
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    request->m_state = state;
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+bool threadpool<T>::append_p(T *request)
+{
+    m_queuelocker.lock();
+    if (m_workqueue.size() >= m_max_requests)
+    {
+        m_queuelocker.unlock();
+        return false;
+    }
+    m_workqueue.push_back(request);
+    m_queuelocker.unlock();
+    m_queuestat.post();
+    return true;
+}
+template <typename T>
+void *threadpool<T>::worker(void *arg)
+{
+    threadpool *pool = (threadpool *)arg;
+    pool->run();
+    return pool;
+}
+template <typename T>
+void threadpool<T>::run()
+{
+    while (true)
+    {
+        m_queuestat.wait();
+        m_queuelocker.lock();
+        if (m_workqueue.empty())
+        {
+            m_queuelocker.unlock();
+            continue;
+        }
+        T *request = m_workqueue.front();
+        m_workqueue.pop_front();
+        m_queuelocker.unlock();
+        if (!request)
+            continue;
+        if (1 == m_actor_model)
+        {
+            if (0 == request->m_state)
+            {
+                if (request->read_once())
+                {
+                    request->improv = 1;
+                    connectionRAII mysqlcon(&request->mysql, m_connPool);
+                    request->process();
+                }
+                else
+                {
+                    request->improv = 1;
+                    request->timer_flag = 1;
+                }
+            }
+            else
+            {
+                if (request->write())
+                {
+                    request->improv = 1;
+                }
+                else
+                {
+                    request->improv = 1;
+                    request->timer_flag = 1;
+                }
+            }
+        }
+        else
+        {
+            connectionRAII mysqlcon(&request->mysql, m_connPool);
+            request->process();
+        }
+    }
+}
+#endif
